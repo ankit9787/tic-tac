b3cdd9efb15736a87b8e7a584db268bc1e63a8c5
Ankit Singh <34910004+ankit9787@users.noreply.github.com>
2023-07-03 17:01:36 +0530

    inital commit

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let symbol = 'X';
 var cells = document.getElementsByClassName('cell');
 
-// Define the event listener function
 function handleCellClick() {
   ticTacSymbol(this);
 }
 
-// Add the event listener to each cell
 Array.from(cells).forEach(function (cell) {
   cell.addEventListener('click', handleCellClick);
 });
@@ -21,14 +19,14 @@ function ticTacSymbol(cell) {
 
 function winCheck() {
   const winningCombinations = [
-    [0, 1, 2], // Top row
-    [3, 4, 5], // Middle row
-    [6, 7, 8], // Bottom row
-    [0, 3, 6], // Left column
-    [1, 4, 7], // Middle column
-    [2, 5, 8], // Right column
-    [0, 4, 8], // Diagonal top-left to bottom-right
-    [2, 4, 6]  // Diagonal bottom-left to top-right
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]  
   ];
 
   for (const combination of winningCombinations) {
